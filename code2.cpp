1199d443d0add3b20e7d376af0bcf849ec9fafe7
Coder <161161399+UserForKAIT@users.noreply.github.com>
2024-02-25 00:44:01 +0300

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,13 @@
+int main() {
+  return 100;
+}
+
+int main() {
+  return -100;
+}
+
+void main() { // type
+  return;
+}
+
+// what ?
---
+++ prog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void func () {  // function
+  cout << "text1";
+  cout << "text2";
+}
+
+int main() {
+  func();  // run function
+
+system ("pause")
+return 0;
+}
